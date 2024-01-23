0b66231466fff12f99b05c4901552b586bf50d70
Brad Snyder <bsnyder13@gmail.com>
2024-01-22 21:10:39 -0500

    added buttons and addition functionality

--- calculator/src/routes/+page.svelte
+++ calculator/src/routes/+page.svelte
@@ -1,59 +1,105 @@
 <script>
-	import Counter from './Counter.svelte';
-	import welcome from '$lib/images/svelte-welcome.webp';
-	import welcome_fallback from '$lib/images/svelte-welcome.png';
+	let display_number = "";
+	const select = (num) => () => (display_number += num);
+	const clear = () => (display_number = "");
+
+	let operand;
+    let result;
+
+    function addition() {
+        operand = Number(display_number);
+        display_number = "";
+    }
+
+    function equals() {
+        display_number = Number(display_number);
+        result = operand + display_number
+        display_number = result.toString()
+    }
+
+	
 </script>
 
-<svelte:head>
-	<title>Home</title>
-	<meta name="description" content="Svelte demo app" />
-</svelte:head>
+<div class="calculator">
+	<div class="display">{display_number}</div>
+	<div class="buttons">
+		<button on:click={select(7)}>7</button>
+		<button on:click={select(8)}>8</button>
+		<button on:click={select(9)}>9</button>
+		<button on:click={addition} class="operator" >+</button>
+		<button on:click={select(4)}>4</button>
+		<button on:click={select(5)}>5</button>
+		<button on:click={select(6)}>6</button>
+		<button on:click={() => operation(operators[1])} class="operator" >-</button>
+		<button on:click={select(1)}>1</button>
+		<button on:click={select(2)}>2</button>
+		<button on:click={select(3)}>3</button>
+		<button class="operator">*</button>
+		<button on:click={select(0)}>0</button>
+		<button on:click={select(".")}>.</button>
+		<button on:click={clear} class="clear">C</button>
+		<button class="operator">/</button>
+		<button class="equals" >=</button>
+	</div>
+</div>
 
-<section>
-	<h1>
-		<span class="welcome">
-			<picture>
-				<source srcset={welcome} type="image/webp" />
-				<img src={welcome_fallback} alt="Welcome" />
-			</picture>
-		</span>
-
-		to your new<br />SvelteKit app
-	</h1>
-
-	<h2>
-		try editing <strong>src/routes/+page.svelte</strong>
-	</h2>
-
-	<Counter />
-</section>
 
 <style>
-	section {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		flex: 0.6;
-	}
+.calculator {
+	display: inline-block;
+	border: 1px solid #ccc;
+	padding: 10px;
+	border-radius: 5px;
+	box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
+}
 
-	h1 {
-		width: 100%;
-	}
+.display {
+	font-size: 24px;
+	border: 1px solid #ccc;
+	height: 24px;
+	padding: 10px;
+	margin-bottom: 10px;
+	width: 300px;
+	text-align: right;
+}
 
-	.welcome {
-		display: block;
-		position: relative;
-		width: 100%;
-		height: 0;
-		padding: 0 0 calc(100% * 495 / 2048) 0;
-	}
+.buttons {
+	display: grid;
+	grid-template-columns: repeat(4, 75px);
+	gap: 5px;
+}
 
-	.welcome img {
-		position: absolute;
-		width: 100%;
-		height: 100%;
-		top: 0;
-		display: block;
-	}
+button {
+	padding: 10px;
+	font-size: 18px;
+	border: none;
+	cursor: pointer;
+	border-radius: 8px;
+	font-family: Arial, sans-serif;
+	text-align: center;
+}
+
+.operator{
+	border: 0px solid #A46D19;
+	background: #F90;
+}
+.clear{
+	border: 0px solid #000;
+	background: red;
+}
+.equals{
+	border: 0px solid #000;
+	background: #245BE9;
+	width: 320px
+}
+
+.operator:hover{
+	background: #FFD700;
+}
+.clear:hover {
+	background: #FFC0CB ;
+}
+.equals:hover {
+	background: #ADD8E6; 
+}
 </style>
